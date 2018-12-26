191fd45414656cabf2969a9ed5f5d165b58e7b6f
Guruprasad Ayyaswamy <9124185+gayyaswa@users.noreply.github.com>
2018-12-25 19:53:36 -0700

    Implement PID Controller
    Implements the PID controller by manually tuning
    the P,I,D parameters respectively

--- src/main.cpp
+++ src/main.cpp
@@ -33,7 +33,7 @@ int main()
   uWS::Hub h;
 
   PID pid;
-  // TODO: Initialize the pid variable.
+  pid.Init(-0.20, 0.0, -1.0); //P, I, D
 
   h.onMessage([&pid](uWS::WebSocket<uWS::SERVER> ws, char *data, size_t length, uWS::OpCode opCode) {
     // "42" at the start of the message means there's a websocket message event.
@@ -51,19 +51,30 @@ int main()
           double speed = std::stod(j[1]["speed"].get<std::string>());
           double angle = std::stod(j[1]["steering_angle"].get<std::string>());
           double steer_value;
-          /*
-          * TODO: Calcuate steering value here, remember the steering value is
-          * [-1, 1].
-          * NOTE: Feel free to play around with the throttle and speed. Maybe use
-          * another PID controller to control the speed!
-          */
           
+          pid.UpdateError(cte);
+          steer_value = pid.TotalError();
+
           // DEBUG
-          std::cout << "CTE: " << cte << " Steering Value: " << steer_value << std::endl;
+          std::cout << "CTE: " << cte << " Steering Value: " << steer_value <<"Speed:" << speed <<std::endl;
+
+          //Initialize with provided default value
+          double throttle = 0.3;
+
+          if( fabs(cte) > 0 && fabs(cte) < 1.0)
+          {
+        	  throttle = throttle * fabs(cte);
+          }
+
+          /*//Adjust the throttle based on steer angle
+          if( fabs(angle) > 0.2)
+          {
+        	  throttle = throttle * fabs(angle);
+          }*/
 
           json msgJson;
           msgJson["steering_angle"] = steer_value;
-          msgJson["throttle"] = 0.3;
+          msgJson["throttle"] = throttle;
           auto msg = "42[\"steer\"," + msgJson.dump() + "]";
           std::cout << msg << std::endl;
           ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
